62396661d0a6aaef31f22f515a81a5509a4ad39d
Pygar04 <101059425+Pygar04@users.noreply.github.com>
2023-02-10 00:29:25 +0100

    Update battagliaNavale.cpp
    Signed-off-by: Pygar04 <101059425+Pygar04@users.noreply.github.com>

--- battagliaNavale.cpp
+++ battagliaNavale.cpp
@@ -1,13 +1,512 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 using namespace std;
+const int N=10;//grandezza mappa
+
+//prototipi
+void menu1(); //Amodeo
+void menu2(); //Canucciari
+void difficile(char difesa1[][N], char attacco1[][N], char difesa2[][N], char attacco2[][N], int N); //Modalità gioco 2 giocatori
+void cpu(char difesa1[][N], char attacco1[][N], char difesa2[][N], char attacco2[][N], int N); //funzione per giocare contro il computer
+bool validaPos(char mappa[][N], int x, int y, int direzione, int nav);//funzione per validare la posizioni delle navi per non piazzarle accanto 
+void inizMappa(char mappa[][N], int N); // inizializza le mappa di gioco
+void stampa(char mappa[][N], int N); // stampa la mappa
+void stampa2(char mappa1[N],char mappa2[][N], int N); // stampa le due mappe
+void piazzaNav(char mappa[][N], int N); // funzione per piazzare le navi nella mappa
+void casualNav(char mappa[][N], int N); // funzione per piazzare le navi nella mappa in modo casuale
+bool affondato(char mappa[][N], int N); // funzione che controll se le nave è stata affondata
+int turno();
+void regole();
+//Amodeo
+void cerca(char M[][N], int N);
+void semplice();
+
+//main
+int main()
+{
+    cout<<"qui";
+    
+    menu1(); 
+    
+    return 0;
+}
 
 //funzioni
 
-
-//main
-int main ()
+//menu
+void menu1()
 {
-  return 0;
+    system("CLS");
+    int scelta;
+    bool esci=false;
+    
+    while (!esci) 
+    {
+        cout<<"Menu:"<<endl;
+        cout<<"1. semplice"<<endl;
+        cout<<"2. Difficile"<<endl;
+        cout<<"3. Esci"<<endl;
+        cout<<"Inserisci il numero della tua scelta: ";
+        cin>>scelta;
+        
+        switch (scelta) 
+        {
+            case 1:
+                semplice();
+                break;
+            case 2:
+                menu2();
+                break;
+            case 3:
+                cout<<"Arrivederci!"<<endl;
+                esci=true;
+                break;
+            default:
+                cout<<"Scelta non valida, riprova."<<endl;
+                break;
+        }
+    }
+}
+
+void menu2()
+{
+    system("CLS");
+    char difesa1[N][N], attacco1[N][N],difesa2[N][N], attacco2[N][N];   
+    int scelta;
+    bool esci=false;
+    
+    while (!esci) 
+    {
+        cout<<"Menu:"<<endl;
+        cout<<"1. 2 giocatori"<<endl;
+        cout<<"2. CPU"<<endl;
+        cout<<"3. Esci"<<endl;
+        cout<<"Inserisci il numero della tua scelta: ";
+        cin>>scelta;
+        
+        switch (scelta)
+        {
+            case 1:
+                difficile(difesa1, attacco1, difesa2 , attacco2, N);
+                break;
+            case 2:
+                cpu(difesa1, attacco1, difesa2 , attacco2, N);
+                break;
+            case 3:
+                cout<<"Arrivederci!" << endl;
+                esci=true;
+                break;
+            default:
+                cout<<"Scelta non valida, riprova."<<endl;
+                break;
+        }
+    }
+}
+
+
+void inizMappa(char mappa[][N], int N)
+{
+    for(int i=0; i < N; i++){
+        for(int j=0; j < N; j++)
+            mappa[i][j]='.';
+    }
+        
+}
+
+//stampa
+void stampa(char mappa[][N], int N)
+{
+    system("cls");
+    cout<<"_______Mappa_______"<<endl;
+    cout<<"0.1.2.3.4.5.6.7.8.9."<<endl;
+    for(int i=0; i < N; i++)
+    {
+        for(int j=0; j < N; j++)
+            cout<<mappa[i][j]<<"|";
+        cout<<endl;
+    }
+}
+
+void stampa2(char mappa1[][N],char mappa2[][N], int N)
+{
+    for (int i=0; i < N; i++) 
+    {
+        for (int j=0; j < N; j++) 
+            cout<<"|"<<mappa1[i][j]<<"|";
+        cout<<setw(10)<<" ";
+        for (int j=0; j < N; j++) 
+            cout<<"|"<<mappa2[i][j]<<"|";
+        cout<<endl;
+    }
+}
+
+void piazzaNav(char mappa[][N], int N)
+{
+    int x, y, direzione,nav;
+    int navi[10]={4, 3, 3, 3, 2, 2, 2, 1, 1, 1};
+    for(int i=0; i < N; i++)
+    {
+        stampa(mappa, N);
+        nav=navi[i];
+        do
+        {
+            cout<<"\nInserisci la posizione della nave da "<<nav<<" (x y (da 0 a 9), orizzontale (0), verticale(1)): ";
+            cin>>x>>y>>direzione;
+            if(x>9 || x <0 || y>9 || y<0 || direzione==0 && direzione == 1 )
+                cout<<"\nLe inforazioni non sono corrette....... Prego insrire di nuovo i dati corretamente......";
+        }while(x<0 || x >9 || y<0 || y>9 || direzione!=0 && direzione != 1 );
+        while (validaPos (mappa,x, y, direzione, nav)) 
+        {
+            do
+            {
+                cout << "Posizione Invalida! La nave non entrera' nella mappa o si scontrera' con un'altra nave.\n";
+                cout<<"\nInserisci la posizione della nave da "<<nav<<" (x y (da 0 a 9), orizzontale (0), verticale(1)): ";
+                cin>>x>>y>>direzione;
+                if(x>9 || x <0 || y>9 || y<0 || direzione == 0 && direzione == 1 )
+                    cout<<"\nLe inforazioni non sono corrette....... Prego inserire di nuovo i dati corretamente......";
+            }while(x<0 || x >9 || y<0 || y>9 || direzione != 0 && direzione != 1 );
+        }
+        for (int i=0; i < nav; i++) 
+        {
+            if (direzione == 0) 
+                mappa[x][y+i]='*'; // orizzontale
+
+            else 
+                mappa[x+i][y]='*'; // verticale
+        }
+    }
+
+}
+
+void casualNav(char mappa[][N], int N)
+{
+    int x, y, direzione,nav;
+    int navi[10]={4, 3, 3, 3, 2, 2, 2, 1, 1, 1};
+    for(int i=0; i < N; i++)
+    {
+        nav=navi[i];
+        do
+        {
+            x=rand()%N;
+            y=rand()%N;
+            direzione=rand()%2;
+
+        }while(x<0 || x >9 || y<0 || y>9 || direzione!=0 && direzione !=1 );
+        while (validaPos(mappa,x, y, direzione, nav)) 
+        {
+            do
+            {
+                x=rand()%N;
+                y=rand()%N;
+                direzione=rand()%2;
+
+            }while(x<0 || x >9 || y<0 || y>9 || direzione!=0 && direzione !=1 );
+        }
+        for (int i=0; i < nav; i++) 
+        {
+            if (direzione == 0) 
+                mappa[x][y+i]='*'; // orizzontale
+            else 
+                mappa[x+i][y]='*'; // verticale
+        }
+    }
+}
+
+bool validaPos(char mappa[][N],int x, int y, int direzione, int nav)
+{
+    for (int i=0; i < nav; i++) 
+    {
+        if (direzione == 0) 
+            if (x+i >= N || mappa[x][y+i] !='.') //orizzontale
+                return true;
+        else 
+            if (y+i >= N || mappa[x+i][y] !='.') // verticale
+                return true;
+    }
+    return false;
+}
+
+void attacco(char mappa1[][N],char mappa2[][N],int x, int y) 
+{
+    if(mappa1[x][y] == '*') 
+    {
+        cout<<"Colpito!"<<endl;
+        mappa2[x][y]='X';
+    } 
+    else
+        {
+            cout<<"Mancato."<<endl;
+            mappa2[x][y]='O';
+        }
+    }
+
+bool affondato(char mappa[][N], int N)
+{
+    int shipCells = 0;
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N; j++) 
+        {
+            if (mappa[i][j] == 'X') 
+                shipCells++;
+        }
+    }
+    return (shipCells == 0);
+}
+
+//cpu
+void cpu(char difesa1[N][N], char attacco1[N][N], char difesa2[N][N], char attacco2[N][N], int N)
+{
+    //inizializza mappa del computer in modo casuale
+    srand(time(0));
+    int x, y, direzione,nav;
+    int navi[10]={4, 3, 3, 3, 2, 2, 2, 1, 1, 1};
+    inizMappa(difesa1, N);
+    inizMappa(attacco1, N);
+    inizMappa(difesa2, N);
+    inizMappa(attacco2, N);
+    casualNav(difesa2, N);
+    system("cls");  
+    int scelta;
+    bool esci=false;
+    while (!esci) 
+    {
+        cout<<"1. Piazzamento navi:"<<endl;
+        cout<<"1. manuale"<<endl;
+        cout<<"2. casuale"<<endl;
+        cout<<"Inserisci il numero della tua scelta: ";
+        cin>>scelta;
+        
+        switch (scelta)
+        {
+            case 1:
+                piazzaNav(difesa1, N);
+                esci=true;
+                break;
+            case 2:
+                casualNav(difesa1, N);
+                esci=true;
+                break;
+            default:
+                cout<<"Scelta non valida, riprova."<<endl;
+                break;
+        }
+    
+    }
+}
+
+// 2 giocatori
+void difficile(char difesa1[][N], char attacco1[][N], char difesa2[][N], char attacco2[][N], int N)
+{
+    system("cls");  
+    int scelta;
+    bool esci=false;
+    inizMappa(difesa1, N);
+    inizMappa(attacco1, N);
+    inizMappa(difesa2, N);
+    inizMappa(attacco2, N);
+    cout<<"Giocatore 1"<<endl;
+    while (!esci) 
+    {
+        cout<<"1. Piazzamento navi:"<<endl;
+        cout<<"1. manuale"<<endl;
+        cout<<"2. casuale"<<endl;
+        cout<<"Inserisci il numero della tua scelta: ";
+        cin>>scelta;
+        switch (scelta)
+        {
+            case 1:
+                piazzaNav(difesa1,N);
+                esci=true;
+                break;
+            case 2:
+                casualNav(difesa1,N);
+                esci=true;
+                break;
+            default:
+                cout<<"Scelta non valida, riprova."<<endl;
+                break;
+        }
+    }
+    scelta=0;
+    esci=false;
+    system("cls");
+    cout<<"Giocatore 2"<<endl;
+    while (!esci) 
+    {
+        cout<<"1. Piazzamento navi:"<<endl;
+        cout<<"1. manuale"<<endl;
+        cout<<"2. casuale"<<endl;
+        cout<<"Inserisci il numero della tua scelta: ";
+        cin>>scelta;
+        
+        switch (scelta)
+        {
+            case 1:
+                piazzaNav(difesa2,N);
+                esci=true;
+                break;
+            case 2:
+                casualNav(difesa2,N);
+                esci=true;
+                break;
+            default:
+                cout<<"Scelta non valida, riprova."<<endl;
+                break;
+        }
+    }
+    int nGiocatore1=10, nGiocatore2=10, x, y;
+    bool turno=true;
+    do
+    {
+        if (turno) 
+        {
+            system("cls");
+            cout<< "Turno del giocatore 1" <<endl<<endl;
+            stampa2(difesa1,attacco1, N);
+            do
+            {
+                cout<<"Inserisci le coordinate per attaccare (x e y (da 0, 9)): "<<endl;
+                cin>>x>>y;
+                if(x>9 || x <0 || y>9 || y<0)
+                    cout<<"\nLe coordinate non sono corrette....... Prego insrire di nuovo i dati corretamente......";
+            }while(x<0 || x >9 || y<0 || y>9);
+            attacco(difesa2,attacco1,x,y);
+            if(affondato(difesa2, N))
+                nGiocatore2--;
+
+            turno = false;
+        } 
+        else 
+        {
+            system("cls");
+            cout<<"Turno del giocatore 2"<<endl<<endl;
+            stampa2(difesa2,attacco2, N);
+            do
+            {
+                cout<<"Inserisci le coordinate per attaccare (x e y (da 0, 9)): "<<endl;
+                cin>>x>>y;
+                if(x>9 || x <0 || y>9 || y<0)
+                    cout<<"\nLe coordinate non sono corrette....... Prego inserire di nuovo i dati corretamente......";
+            }while(x<0 || x >9 || y<0 || y>9);
+            attacco(difesa1,attacco2,x,y);
+            if(affondato(difesa1, N))
+                nGiocatore1--;
+            turno = true;
+        }  
+    } while (nGiocatore1 > 0 && nGiocatore2 > 0); // Continua il ciclo finché entrambi i giocatori hanno ancora navi
+
+    if (nGiocatore1 > 0) 
+        cout<<"Il giocatore 1 ha vinto!"<<endl;
+    else  
+        if (nGiocatore2 > 0) 
+            cout<<"Il giocatore 2 ha vinto!"<<endl;
+        else 
+            cout<<"La partita e' terminata in pareggio."<<endl;        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Amodeo
+void semplice()
+{
+    char M[][N]={
+        {'*','.','.','.','.','.','.','.','*','.'},
+        {'.','.','.','.','.','.','.','.','.','.'},
+        {'.','.','.','*','.','.','.','.','.','*'},
+        {'.','.','.','.','.','.','.','.','.','*'},
+        {'.','.','.','.','*','.','.','.','.','.'},
+        {'.','.','.','.','*','.','.','*','.','*'},
+        {'.','*','.','.','*','.','.','*','.','*'},
+        {'.','.','.','.','.','.','.','*','.','.'},
+        {'.','.','.','.','.','.','.','.','.','*'},
+        {'.','.','.','*','*','*','*','.','.','*'},
+    };
+    stampa(M,N);       
+    cerca(M, N);
+}
+
+void cerca(char M[][N], int N)
+{
+    int r, c, r2, c2;
+    int numero=0;
+    for(int i=0; i < N; i++){
+        numero=0;
+        for(int j=0; j < N; j++)
+        {
+            
+            if(M[i][j]=='*')
+            {
+                numero++;
+               
+            }
+            if(numero==1)
+            {
+                r=i;
+                c=j;
+            }
+            if(numero==4)
+            {
+                r2=i;
+                c2=j;
+                i=N;
+                j=N;
+            }
+            if(M[i][j]=='.')
+            {
+                numero=0;
+            }
+            cout<<endl;
+        }
+    }
+       
+    if(numero==4)
+        cout<<"\nLa nave da 4 caselle inizia dalla cordinata : ["<<r<<"],["<<c<<"].... e finisce a : ["<<r2<<"],["<<c2<<"]";
+    else
+        cout<<"\nNon e' stata trovata nessuna nave lunga 4 caselle in orizzontale\n"; 
+      
+    for(int i=0; i < N; i++)
+    {
+        numero=0;
+        for(int j=0; j < N; j++)
+        {
+            if(M[j][i]=='*')
+                numero++;
+            if(numero==1)
+            {
+                r=j;
+                c=i;
+            }
+            if(numero==4)
+            {
+                r2=j;
+                c2=i;
+                i=N;
+                j=N;
+            }
+            if(M[j][i]=='.')
+                numero=0;
+        }
+    }
+        
+    if(numero==4)
+        cout<<"\nLa nave da 4 caselle inizia dalla cordinata : ["<<r<<"],["<<c<<"].... e finisce a : ["<<r2<<"],["<<c2<<"]\n";
+    else
+        cout<<"\nNon e' stata trovata nessuna nave lunga 4 caselle in verticale\n";
 }
